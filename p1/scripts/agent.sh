f9a3dc0768cad07eb4f254d690d005ba1e157cac
Thibaud Paquier <Thibaud@hawaii.do>
2024-08-16 13:25:17 +0200

    ADD script for master and agent

---
+++ p1/scripts/agent.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Ensure the token file exists (waiting for the server to generate it)
+while [ ! -f /vagrant/k3s_token ]; do
+    echo "Waiting for k3s token..."
+    sleep 5
+done
+
+# Read the token from the file
+K3S_TOKEN=$(cat /vagrant/k3s_token)
+
+# Install k3s as the agent node and join the server
+curl -sfL https://get.k3s.io | K3S_URL=https://192.168.56.110:6443 K3S_TOKEN=$K3S_TOKEN sh -
+
+echo "k3s agent installation completed."
---
+++ p1/scripts/master.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Install k3s as the server
+curl -sfL https://get.k3s.io | sh -
+
+# Wait for k3s to be fully up and running
+echo "Wait for k3s server to start..."
+sleep 35
+
+# Output the node status to verify the server is up
+sudo k3s kubectl get node
+
+# Output the k3s token to a file so the agent node can use it to join
+sudo cat /var/lib/rancher/k3s/server/node-token > /vagrant/k3s_token
+
+echo "k3s server installation completed."
