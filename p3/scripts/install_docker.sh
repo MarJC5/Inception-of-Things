eb990ce96894016ba398ca95fc5179047e304e17
Jean-Christio Martin <admin@marjc.ch>
2025-03-04 22:42:50 +0100

    Update Docker installation script for Ubuntu

--- p3/scripts/install_docker.sh
+++ p3/scripts/install_docker.sh
@@ -21,12 +21,12 @@ sudo apt install -y \
 # Add Docker's official GPG key
 echo "Adding Docker's GPG key..."
 sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
 # Add Docker's official repository
 echo "Adding Docker's repository..."
 echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 # Update package list to include Docker's repository
