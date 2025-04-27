32ddb619daa73b5114b2d419ea72575a5d808036
Scelewyn <Scelewyn@gmail.com>
2025-04-28 00:51:40 +0200

    Better delay and config rename

--- p2/scripts/master.sh
+++ p2/scripts/master.sh
@@ -2,17 +2,12 @@
 
 # Install k3s as the server
 curl -sfL https://get.k3s.io | sh -
+until sudo k3s kubectl get node mpouceS | grep " Ready " &> /dev/null
+do
+    echo "Waiting for node to become Ready..."
+    sleep 5
+done
 
-# Wait for k3s to be fully up and running
-echo "Wait for k3s server to start..."
-sleep 35
-
-# Output the node status to verify the server is up
-sudo k3s kubectl get node
-
-# Output the k3s token to a file so the agent node can use it to join
-sudo cat /var/lib/rancher/k3s/server/node-token > /vagrant/k3s_token
-
-sudo kubectl apply -f '/vagrant/config/*'
+sudo kubectl apply -f '/vagrant/confs/*'
 
 echo "k3s server installation completed."
